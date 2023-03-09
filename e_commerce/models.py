efef980cd5cd9d13184117776b60c63102b5ac8d
jdarangom2012 <jdarangom2012@gmail.com>
2023-03-09 11:26:56 -0500

    ModeloDbMod3

---
+++ e_commerce/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+# NOTE: Para poder utilizar el modelo "user" que viene por defecto en Django,
+# Debemos importarlo previamente:
+# from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Comic(models.Model):
+    '''
+    Esta clase hereda de Django models.Model y crea una tabla llamada
+    e_commerce_comic. Las columnas toman el nombre especificado de cada objeto.
+    '''
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    marvel_id = models.PositiveIntegerField(
+        verbose_name='marvel id', null=False, blank=False, unique=True
+    )
+    title = models.CharField(
+        verbose_name='title', max_length=120, default=''
+    )
+    description = models.TextField(verbose_name='description', default='')
+    price = models.FloatField(
+        verbose_name='price', max_length=5, default=0.00
+    )
+    stock_qty = models.PositiveIntegerField(
+        verbose_name='stock qty', default=0
+    )
+    picture = models.URLField(verbose_name='picture', default='')
+
+    class Meta:
+        '''
+        Con "class Meta" podemos definir atributos de nuestras entidades como el nombre de la tabla.
+        '''
+        db_table = 'e_commerce_comics'
+        verbose_name = 'comic'
+        verbose_name_plural = 'comics'
+
+    def __str__(self):
+        '''
+        El método __str__ cumple una función parecida a __repr__ en SQL Alchemy, 
+        es lo que retorna cuando llamamos al objeto.
+        '''
+        return f'{self.id}'
+
+    class WishList(models.Model):
+        id = models.BigAutoField(db_column='ID', primary_key=True)
+        #user = models.ForeignKey(User,null=False,blank=False, on_delete=models.CASCADE)
+        IdComic = models.ForeignKey("Comic", verbose_name=("IdComic"), on_delete=models.CASCADE)
+        favorite = models.CharField(verbose_name="favorite" , max_length=50, default="")
+        cart = models.PositiveIntegerField(verbose_name="cart", default=0)
+        wished_qty = models.PositiveIntegerField(verbose_name="wished_qty", default=0)
+        bought_qty = models.PositiveIntegerField(verbose_name="bought_qty", default=0)
+
+    class Meta:        
+        db_table = 'wish_list'
+        verbose_name = 'wish_list'
+        
+    def __str__(self):
+        return f'{self.id}'
+
+    
